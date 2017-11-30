e918ede89f44199223da5934bf43821845083f36
Allen0003 <apss1943@gmail.com>
2017-11-30 16:31:35 +1100

    Add files via upload

---
+++ ckeditor demo/vendor/ckeditor/config.js
@@ -0,0 +1,7 @@
+CKEDITOR.editorConfig = function(config) {
+  // Define changes to default configuration here.
+  config.toolbarGroups = [{
+    name: 'basicstyles',
+    groups: ['basicstyles', 'cleanup']
+  }, ];
+};
